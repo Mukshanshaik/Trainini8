d7ad0956d3dc9a473682913bf726e7bb53579d5a
Muskhan <149375654+Mukshanshaik@users.noreply.github.com>
2024-10-06 20:55:51 +0530

    Add files via upload

---
+++ StartupTraining8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Statup.Training.Center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTraining8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
