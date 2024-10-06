06cd60f5bb023df3bc531972a9448fa0864535ea
Muskhan <149375654+Mukshanshaik@users.noreply.github.com>
2024-10-06 20:54:15 +0530

    Add files via upload

---
+++ TrainingCenterRepository.java
@@ -0,0 +1,12 @@
+package com.Startup.Training.Center.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Startup.Training.Center.Entity.TrainingCenter;
+
+public interface TrainingCenterRepository extends JpaRepository<TrainingCenter, Long>
+{
+
+
+
+}
